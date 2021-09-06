d28a920352c734125fa5f4af7d83712a315f20b0
umer mushtaq <umermushtaq3424@gmail.com>
2021-09-06 22:22:05 +0500

    navbar

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,18 +1,8 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  constructor(private _http: HttpClient) {}
-
-  title = 'public';
-  ngOnInit(): void {
-    this._http
-      .get('http://localhost:8888/api/students/')
-      .subscribe((res) => console.log(res));
-  }
-}
+export class AppComponent {}
--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -4,10 +4,19 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, HeaderComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
