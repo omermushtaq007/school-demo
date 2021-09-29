e0199831892a9bc4ff0dc3470c95b7ecca5ef61f
umer mushtaq <umermushtaq3424@gmail.com>
2021-09-29 23:07:43 +0500

    menu navbar

--- public/src/app/form/form.module.ts
+++ public/src/app/form/form.module.ts
@@ -1,4 +1,4 @@
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
@@ -6,17 +6,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RegisterComponent } from './register/register.component';
 import { MaterialModule } from '../material/material.module';
 
-const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-];
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [
-    CommonModule,
-    RouterModule,
-    ReactiveFormsModule,
-    MaterialModule,
-  ],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule, MaterialModule],
 })
 export class FormModule {}
--- public/src/app/main/main.module.ts
+++ public/src/app/main/main.module.ts
@@ -5,9 +5,10 @@ import { MaterialModule } from '../material/material.module';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main.component';
 import { FooterComponent } from './footer/footer.component';
+import { MenuHeaderComponent } from './menu-header/menu-header.component';
 
 @NgModule({
-  declarations: [HeaderComponent, MainComponent, FooterComponent],
+  declarations: [HeaderComponent, MainComponent, FooterComponent, MenuHeaderComponent],
   imports: [CommonModule, MaterialModule, RouterModule],
   exports: [MainComponent],
 })
